b4cbcbb23ddd26659629c678621a5e73e7d1cbcf
Wayne Werner <wayne@waynewerner.com>
2014-01-13 20:13:44 -0600

    Got setup.py tweaked & test running

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import sys
 
 try:
     from setuptools import setup
+    from setuptools.command.test import test as TestCommand
 except ImportError:
     from distutils.core import setup
 
@@ -17,8 +18,19 @@ if sys.argv[-1] == 'publish':
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 setup(
-    name='draftin-a-flask',
+    name='draftin_a_flask',
     version='0.1.0',
     description='A simple Flask server that allows you to publish Pelican blags from http://draftin.com',
     long_description=readme + '\n\n' + history,
@@ -26,15 +38,16 @@ setup(
     author_email='waynejwerner@gmail.com',
     url='https://github.com/waynew/draftin-a-flask',
     packages=[
-        'draftin-a-flask',
+        'draftin_a_flask',
     ],
-    package_dir={'draftin-a-flask': 'draftin-a-flask'},
+    cmdclass={'test': PyTest},
+    package_dir={'draftin_a_flask': 'draftin_a_flask'},
     include_package_data=True,
     install_requires=[
     ],
     license="BSD",
     zip_safe=False,
-    keywords='draftin-a-flask',
+    keywords='draft flask pelican blog publish',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -47,4 +60,5 @@ setup(
         'Programming Language :: Python :: 3.3',
     ],
     test_suite='tests',
+    tests_require=['pytest'],
 )
---
+++ tests/test_draftin_a_flask.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_draftin-a-flask
+----------------------------------
+
+Tests for `draftin-a-flask` module.
+"""
+
+import pytest
+
+from draftin_a_flask import draftin_a_flask
+
+def test_it_works():
+    assert False
