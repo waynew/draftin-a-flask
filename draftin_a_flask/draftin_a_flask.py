f5e97cfdd8c0d6cac0b818b8e14c882c90c788e1
Wayne Werner <wayne@waynewerner.com>
2014-01-13 20:39:58 -0600

    First green test - creating key file

--- draftin_a_flask/draftin_a_flask.py
+++ draftin_a_flask/draftin_a_flask.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
+def setup():
+    root = os.path.dirname(os.path.abspath(__file__))
+    secret_file = os.path.join(root, 's3kret.key')
+    if not os.path.isfile(secret_file):
+        with open(secret_file, 'w') as f:
+            f.write('')
--- tests/test_draftin_a_flask.py
+++ tests/test_draftin_a_flask.py
@@ -2,15 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """
-test_draftin-a-flask
+test_draftin_a_flask
 ----------------------------------
 
-Tests for `draftin-a-flask` module.
+Tests for `draftin_a_flask` module.
 """
 
 import pytest
+import os
 
 from draftin_a_flask import draftin_a_flask
 
-def test_it_works():
-    assert False
+def test_on_server_setup_if_secret_keyfile_is_missing_it_should_create():
+    package_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    secret_file = os.path.join(package_root, 'draftin_a_flask', 's3kret.key')
+    if os.path.isfile(secret_file):
+        os.unlink(secret_file)
+    assert not os.path.exists(secret_file)
+
+    draftin_a_flask.setup()
+
+    assert os.path.isfile(secret_file)
